bc6876f0258c7cb70a62ffe39ad33a285c231c49
jonhealy1 <jonathan.d.healy@gmail.com>
2020-07-05 11:01:31 -0700

    add resume, static images

--- personal_portfolio-project/blog/views.py
+++ personal_portfolio-project/blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+    blogs = Blog.objects.order_by('-date')[:5]
+    return render(request, 'blog/all_blogs.html', {'blogs':blogs})
+
+def detail(request, blog_id):
+    return render(request, 'blog/detail.html', {'id':blog_id})
+
